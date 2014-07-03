c25b0cfe11522878a1767f95307bc2ebbb2f1d3e
hubert <hubert@192.168.1.201>
2014-07-02 22:23:28 -0400

    Add formatting and the ability to increment the count associated with a StemEntry by something other than 1.

--- src/main/java/com/mycompany/lexicaldiscriminator/start/StemEntryFrequencyMap.java
+++ src/main/java/com/mycompany/lexicaldiscriminator/start/StemEntryFrequencyMap.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.mycompany.lexicaldiscriminator.start;
 
 import java.util.Iterator;
@@ -12,38 +11,40 @@ import java.util.Map;
 /**
  *
  * @author hubert
- * @param <StemEntry>
- * @param <Integer>
  */
-public class StemEntryFrequencyMap extends java.util.HashMap{
-	public StemEntryFrequencyMap(){
+public class StemEntryFrequencyMap extends java.util.HashMap<StemEntry, Integer> {
+
+	public StemEntryFrequencyMap() {
 		super();
 	}
-	
-	public void add(StemEntry stemEntry){
-		if(this.containsKey(stemEntry)){
-			Integer oldValue = (Integer)this.get(stemEntry);
-			this.put(stemEntry, 1 + oldValue);
-		} else {
-			this.put(stemEntry, 1);
-		}
-		
+
+	public void add(StemEntry stemEntry) {
+		this.add(stemEntry, 1);
 	}
 	
+	public void add(StemEntry stemEntry, Integer anotherCount) {
+		if (this.containsKey(stemEntry)) {
+			Integer oldValue = (Integer) this.get(stemEntry);
+			this.put(stemEntry, anotherCount + oldValue);
+		} else {
+			this.put(stemEntry, anotherCount);
+		}
+	}
+
 	@Override
-	public String toString(){
+	public String toString() {
 		String output = "{";
 		Iterator it = this.entrySet().iterator();
-		while(it.hasNext()){
-			Map.Entry<StemEntry, Integer> ent = (Map.Entry<StemEntry, Integer>)it.next();
+		while (it.hasNext()) {
+			Map.Entry<StemEntry, Integer> ent = (Map.Entry<StemEntry, Integer>) it.next();
 			output = output + "\n[" + ent.getKey().toString() + " | " + ent.getValue() + "]";
 		}
 		output = output + "\n}";
 		return output;
 	}
-	
-	public void main(String[] args){
+
+	public void main(String[] args) {
 
 	}
-	
+
 }
