6a270388228948b44fb5ab81a31ef2ae1246d955
Hubert <hepaces89@gmail.com>
2017-06-24 07:57:52 -0400

    Fix typo in comment

--- src/main/java/com/mycompany/lexicaldiscriminator/TFIDF/TFIDFStemEntryRunner.java
+++ src/main/java/com/mycompany/lexicaldiscriminator/TFIDF/TFIDFStemEntryRunner.java
@@ -23,6 +23,6 @@ public class TFIDFStemEntryRunner extends StemEntryRunner{
 	@Override
 	protected void processStemEntry(StemEntryFrequencyMap[] stemEntryFrequencyMaps, StemEntry stemEntry) {
 		stemEntryFrequencyMaps[0].add(stemEntry);//this increases the count in the Frequency map (counts how many times this stem showed up)
-		stemEntryFrequencyMaps[1].put(stemEntry, 1);//this sets the presence count to 1 for thsi particular topic, indicates that this stem was found
+		stemEntryFrequencyMaps[1].put(stemEntry, 1);//this sets the presence count to 1 for this particular topic, indicates that this stem was found
 	}
 }
